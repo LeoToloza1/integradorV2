1f53e2720c0608fd0795e4d557aae5d09a2984a2
Toloza  Leonel <leotoloza6@gmail.com>
2023-09-18 20:43:28 -0300

    variables de entorno

--- integrador/app.js
+++ integrador/app.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+
+export const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://web2-integrador.onrender.com'
+    : '/';
 import express from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
@@ -6,31 +12,24 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import createError from 'http-errors'; // Importa createError
 import router from "./rutas/rutas.js";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
-export const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://web2-integrador.onrender.com'
-    : '/';
-
 app.use(baseUrl, router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err.message }); // Enviar un mensaje de error JSON
 });
